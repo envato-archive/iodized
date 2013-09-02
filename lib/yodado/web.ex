af3fa9f2182b46a2ede3856494d7757be8883387
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-02 21:01:06 +1000

    tidy up

--- lib/yodado/web.ex
+++ lib/yodado/web.ex
@@ -1,16 +1,19 @@
 defmodule Yodado.Web do
   use Application.Behaviour
 
+  @handler_count 100
+  @port 8080
+
   def start(_type, _args) do
     routes = [
-          {"/", :cowboy_static, [ directory: {:priv_dir, :yodado, ["static"]}, file: "index.html" ]},
-          {"/hello/", Yodado.Web.HelloWorldHandler, []},
-          {"/static/[...]", :cowboy_static, [ directory: {:priv_dir, :yodado, "static"}]}
-        ]
+      {"/", :cowboy_static, [ directory: {:priv_dir, :yodado, ["static"]}, file: "index.html" ]},
+      {"/hello/", Yodado.Web.HelloWorldHandler, []},
+      {"/static/[...]", :cowboy_static, [ directory: {:priv_dir, :yodado, "static"}]}
+    ]
+
     dispatch = [ {:_, routes } ] |> :cowboy_router.compile
-    {:ok, _} = :cowboy.start_http(:http, 100,
-                                  [port: 8080],
-                                  [env: [dispatch: dispatch]])
+
+    {:ok, _} = :cowboy.start_http( :http, @handler_count, [port: @port], [env: [dispatch: dispatch]])
     {:ok, self}
   end
 end
