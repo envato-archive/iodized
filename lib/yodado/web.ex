879bb1c6d7bc8977abb76945d6049c4a5259e8d5
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-02 20:55:36 +1000

    render static files

--- lib/yodado/web.ex
+++ lib/yodado/web.ex
@@ -2,9 +2,12 @@ defmodule Yodado.Web do
   use Application.Behaviour
 
   def start(_type, _args) do
-    dispatch = :cowboy_router.compile([
-                 {:_, [{"/", Yodado.Web.HelloWorldHandler, []}]}
-               ])
+    routes = [
+          {"/", :cowboy_static, [ directory: {:priv_dir, :yodado, ["static"]}, file: "index.html" ]},
+          {"/hello/", Yodado.Web.HelloWorldHandler, []},
+          {"/static/[...]", :cowboy_static, [ directory: {:priv_dir, :yodado, "static"}]}
+        ]
+    dispatch = [ {:_, routes } ] |> :cowboy_router.compile
     {:ok, _} = :cowboy.start_http(:http, 100,
                                   [port: 8080],
                                   [env: [dispatch: dispatch]])
