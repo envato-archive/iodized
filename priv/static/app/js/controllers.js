3495afa9ba9b958b9de3840c7497b4f6fe0c4650
Anthony Sellitti <anthony.sellitti@envato.com>
2013-09-03 16:58:00 +1000

    feature page doneish

--- priv/static/app/js/controllers.js
+++ priv/static/app/js/controllers.js
@@ -3,8 +3,27 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('ViewCtrl', [function() {
-
+  controller('ViewCtrl', ['$scope', function(sc) {
+    sc.features = [
+      {
+         "title"                : "droids_were_looking_for"
+        ,"description"          : "You can go about your business. Move along"
+        ,"state"                : true
+        ,"master_switch_state"  : true
+      }
+      ,{
+         "title"                : "engage_hyper_drive"
+        ,"description"          : "If I may say so, sir, I noticed earlier the hyperdrive motivator has been damaged. It's impossible to go to lightspeed"
+        ,"state"                : true
+        ,"master_switch_state"  : false
+      }
+      ,{
+         "title"                : "open_the_blast_doors"
+        ,"description"          : "Close the blast doors! Open the blast doors! Open the blast doors!"
+        ,"state"                : false
+        ,"master_switch_state"  : true
+      }
+    ];
   }])
   .controller('NewCtrl', [function() {
 
