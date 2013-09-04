ccba3c20897cb906c9fd83990490e9f90d745f2a
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-04 11:39:55 +1000

    make definition records public

--- lib/yodado/model/feature.ex
+++ lib/yodado/model/feature.ex
@@ -1,9 +1,9 @@
 defmodule Yodado.Model.Feature do
 
-  defrecordp :any, options: []
-  defrecordp :all, options: []
-  defrecordp :included_in, name: nil, allowed_values: []
-  defrecordp :is, name: nil, allowed_value: "true"
+  defrecord Any, options: []
+  defrecord All, options: []
+  defrecord IncludedIn, name: nil, allowed_values: []
+  defrecord Is, name: nil, allowed_value: "true"
   
   def do?(feature_id, state) do
     d = definition(feature_id)
@@ -11,46 +11,46 @@ defmodule Yodado.Model.Feature do
   end
 
   def definition(_feature_id) do
-    any(options: [
-      all(options: [
-        included_in(name: "username", allowed_values: ["madlep", "gstamp"]),
-        included_in(name: "host", allowed_values: ["themeforest.net", "codecanyon.net"])
-      ]),
-      is(name: "session_on"),
-      included_in(name: "role", allowed_values: ["developer"])
-    ])
+    Any[options: [
+      All[options: [
+        IncludedIn[name: "username", allowed_values: ["madlep", "gstamp"]],
+        IncludedIn[name: "host", allowed_values: ["themeforest.net", "codecanyon.net"]]
+      ]],
+      Is[name: "session_on"],
+      IncludedIn[name: "role", allowed_values: ["developer"]]
+    ]]
   end
 
-  defp handle_definition(any(options: options), state) do
+  defp handle_definition(Any[options: options], state) do
     options |> Enum.any?(&handle_definition(&1, state))
   end
 
-  defp handle_definition(all(options: options), state) do
+  defp handle_definition(All[options: options], state) do
     options |> Enum.all?(&handle_definition(&1, state))
   end
 
-  defp handle_definition(included_in(name: name, allowed_values: allowed_values), state) do
+  defp handle_definition(IncludedIn[name: name, allowed_values: allowed_values], state) do
     value = state[name]
     Enum.member?(allowed_values, value)
   end
 
-  defp handle_definition(is(name: name, allowed_value: allowed_value), state) do
+  defp handle_definition(Is[name: name, allowed_value: allowed_value], state) do
     state[name] == allowed_value
   end
 
-  def to_json(any(options: options)) do
+  def to_json(Any[options: options]) do
     [any: [options: Enum.map(options, &to_json/1)]]
   end
 
-  def to_json(all(options: options)) do
+  def to_json(All[options: options]) do
     [all: [options: Enum.map(options, &to_json/1)]]
   end
 
-  def to_json(included_in(name: name, allowed_values: allowed_values)) do
+  def to_json(IncludedIn[name: name, allowed_values: allowed_values]) do
     [included_in: [name: name, allowed_values: allowed_values]]
   end
 
-  def to_json(is(name: name, allowed_value: allowed_value)) do
+  def to_json(Is[name: name, allowed_value: allowed_value]) do
     [is: [name: name, allowed_value: allowed_value]]
   end
 end
