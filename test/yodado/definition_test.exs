989c0498f9f16ee966d4b6d1c487dcce50a818a9
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-05 09:31:11 +1000

    test for Any

--- test/yodado/definition_test.exs
+++ test/yodado/definition_test.exs
@@ -1,32 +1,65 @@
 defmodule Yodado.DefinitionTest do
   use ExUnit.Case
+  alias Yodado.Definition.Rule, as: Rule
 
   defmodule AllTest do
     use ExUnit.Case
+    alias Yodado.Definition.All, as: All
 
     test "is true if all conditions match" do
-      rule = Yodado.Definition.All[conditions: [true, true]]
-      assert Yodado.Definition.Rule.matches?(rule, [])
+      rule = All[conditions: [true, true]]
+      assert Rule.matches?(rule, [])
     end
 
     test "is false unless all conditions match" do
-      rule = Yodado.Definition.All[conditions: [true, false]]
-      assert !Yodado.Definition.Rule.matches?(rule, [])
+      rule = All[conditions: [true, false]]
+      assert !Rule.matches?(rule, [])
     end
 
     test "it passes the state through to child conditions" do
-      rule = Yodado.Definition.All[conditions: [
+      rule = All[conditions: [
         fn(state) -> state[:foo] == 1 end,
         fn(state) -> state[:bar] == 2 end
       ]]
       state = [foo: 1, bar: 2]
-      assert Yodado.Definition.Rule.matches?(rule, state)
+      assert Rule.matches?(rule, state)
     end
 
     test "it generates JSON" do
-      rule = Yodado.Definition.All[conditions: [true, false]]
+      rule = All[conditions: [true, false]]
       expected_json = "{\"operand\":\"all\",\"conditions\":[{\"operand\":\"boolean\",\"value\":true},{\"operand\":\"boolean\",\"value\":false}]}"
-      actual_json = Yodado.Definition.Rule.json(rule) |> JSEX.encode!
+      actual_json = Rule.json(rule) |> JSEX.encode!
+      assert actual_json == expected_json
+    end
+  end
+  
+  defmodule AnyTest do
+    use ExUnit.Case
+    alias Yodado.Definition.Any, as: Any
+
+    test "is true if any conditions match" do
+      rule = Any[conditions: [false, true]]
+      assert Rule.matches?(rule, [])
+    end
+
+    test "is false unless any conditions match" do
+      rule = Any[conditions: [false, false]]
+      assert !Rule.matches?(rule, [])
+    end
+
+    test "it passes the state through to child conditions" do
+      rule = Any[conditions: [
+        fn(state) -> state[:foo] == 1 end,
+        fn(state) -> state[:bar] == 2 end
+      ]]
+      state = [foo: 1, bar: -666]
+      assert Rule.matches?(rule, state)
+    end
+
+    test "it generates JSON" do
+      rule = Any[conditions: [true, false]]
+      expected_json = "{\"operand\":\"any\",\"conditions\":[{\"operand\":\"boolean\",\"value\":true},{\"operand\":\"boolean\",\"value\":false}]}"
+      actual_json = Rule.json(rule) |> JSEX.encode!
       assert actual_json == expected_json
     end
   end
