6c900f79772c5d238dc336f9af47690570132989
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-05 21:09:25 +1000

    add feature record with JSON encode/decode

--- lib/yodado/feature.ex
+++ lib/yodado/feature.ex
@@ -1,9 +1,36 @@
 defmodule Yodado.Feature do
+  defrecord Feature, title: nil, description: nil, master_switch_state: nil, definition: nil
   
+  def json(feature) do
+    [
+      title: feature.title,
+      description: feature.description,
+      master_switch_state: feature.master_switch_state,
+      definition: Yodado.Definition.Rule.json(feature.definition)
+    ]
+  end
+
+  def from_json(json) do
+    title = Keyword.fetch!(json, :title)
+    description = Keyword.fetch!(json, :description)
+    master_switch_state = Keyword.fetch!(json, :master_switch_state)
+    definition = Keyword.fetch!(json, :definition) |> Yodado.Definition.from_json
+    Feature[
+      title: title,
+      description: description,
+      master_switch_state: master_switch_state,
+      definition: definition
+    ]
+  end
+
   def do?(feature_id, state) do
     d = definition(feature_id)
     d |> Yodado.Definition.Rule.json |> JSEX.encode!([:indent]) |> IO.puts
     {:ok, Yodado.Definition.Rule.matches?(d, state)}
+  end
+
+  def feature(feature_id) do
+    Feature[title: "I am a feature", description: "yodalol quote here", definition: definition(feature_id)]
   end
 
   def definition(_feature_id) do
