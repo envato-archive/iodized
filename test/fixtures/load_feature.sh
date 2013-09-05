5cd530da73a6d1fdaf81a4f05c2f3d2e4dd49ff2
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-05 23:37:06 +1000

    make script work from anywhere

--- test/fixtures/load_feature.sh
+++ test/fixtures/load_feature.sh
@@ -1,1 +1,1 @@
-curl -X PUT -H 'Content-Type:application/json' -v -d @example_feature.json 'http://localhost:8080/admin/api/feature/i_like_cheese'
+curl -X PUT -H 'Content-Type:application/json' -v -d @`dirname $0`/example_feature.json 'http://localhost:8080/admin/api/feature/i_like_cheese'
