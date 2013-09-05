c29a9acb2b6a24d07578e0192cbb0aee6fcd3be1
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-06 00:38:31 +1000

    hook up definition check logic to redis

--- lib/yodado/feature.ex
+++ lib/yodado/feature.ex
@@ -23,24 +23,8 @@ defmodule Yodado.Feature do
     ]
   end
 
-  def do?(feature_id, state) do
-    d = definition(feature_id)
-    d |> Yodado.Definition.Rule.json |> JSEX.encode!([:indent]) |> IO.puts
-    {:ok, Yodado.Definition.Rule.matches?(d, state)}
-  end
-
-  def feature(feature_id) do
-    Feature[title: "I... am a, feature", description: "yodalol quote here", definition: definition(feature_id)]
-  end
-
-  def definition(_feature_id) do
-    Yodado.Definition.Any[conditions: [
-      Yodado.Definition.All[conditions: [
-        Yodado.Definition.IncludedIn[param_name: "username", value: ["madlep", "gstamp"]],
-        Yodado.Definition.IncludedIn[param_name: "host", value: ["themeforest.net", "codecanyon.net"]]
-      ]],
-      Yodado.Definition.Is[param_name: "session_on"],
-      Yodado.Definition.IncludedIn[param_name: "role", value: ["developer"]]
-    ]]
+  def do?(feature, state) do
+    match = feature.definition |> Yodado.Definition.Rule.matches? state
+    {:ok, match}
   end
 end
--- lib/yodado/web/feature_status_handler.ex
+++ lib/yodado/web/feature_status_handler.ex
@@ -3,8 +3,26 @@ defmodule Yodado.Web.FeatureStatusHandler do
     {:upgrade, :protocol, :cowboy_rest}
   end
 
+  def rest_init(req, opts) do
+    {:ok, req, []}
+  end
+
   def allowed_methods(req, state) do
     {["GET"], req, state}
+  end
+
+  def service_available(req, state) do
+    persistence_ok = Yodado.FeaturePersistence.ping
+    {persistence_ok, req, state}
+  end
+
+  def resource_exists(req, state) do
+    {feature_id, req} = :cowboy_req.binding(:feature_id, req)
+    case Yodado.FeaturePersistence.find_feature(feature_id) do
+      :not_found ->     {false, req, state}
+      {:ok, feature} -> state = Keyword.put(state, :feature, feature)
+                        {true, req, state}
+    end
   end
 
   def content_types_provided(req, state) do
@@ -15,17 +33,12 @@ defmodule Yodado.Web.FeatureStatusHandler do
     {providers, req, state}
   end
 
-  def resource_exists(req, state) do
-    {true, req, state}
-  end
-
   def render_feature_state(req, state) do
     {params, req} = :cowboy_req.qs_vals(req)
 
     req = :cowboy_req.set_resp_header("content-type", "application/json; charset=utf-8", req)
-    {feature_id, req} = :cowboy_req.binding(:feature_id, req)
 
-    {:ok, result} = Yodado.Feature.do?(feature_id, params)
+    {:ok, result} = Yodado.Feature.do?(state[:feature], params)
     body = [state: result] |> JSEX.encode!
 
     {body, req, state}
