36045664e1cee705131c8e983cffa8ebbdaad404
Julian Doherty (madlep) <madlep@madlep.com>
2013-09-17 22:06:51 +1000

    refuctoring stuff still

--- lib/yodado.ex
+++ lib/yodado.ex
@@ -2,21 +2,17 @@ defmodule Yodado do
   use Application.Behaviour
 
   def start(_type, _args) do
-    start_eredis
-  end
-
-  defp start_eredis do
     case :eredis.start_link() do
-      {:ok, redis_pid}  -> {:ok, Process.register(redis_pid, :redis)}
-                           start_web()
-      {:error, error}   -> {:error, "Problem starting eredis error=#{inspect error}"}
+      {:ok, redis_pid}  ->  {:ok, Process.register(redis_pid, :redis)}
+                            start_web()
+      {:error, error}   ->  {:error, "Problem starting eredis error=#{inspect error}"}
     end
   end
 
   defp start_web() do
     case Yodado.Web.start() do
       {:ok, _cowboy_pid} -> {:ok, self()}
-      {:error, error} ->  {:error, error}
+      {:error, error} ->  {:error, "Problem staring cowboy error=#{error}"}
     end
   end
 
