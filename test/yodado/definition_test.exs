e090069e87e1dcc3d3118b57d2e8981a802a6cef
Pete Johns <paj-github@johnsy.com>
2014-03-18 15:39:05 +1100

    Get tests running once more

--- test/yodado/definition_test.exs
+++ test/yodado/definition_test.exs
@@ -7,17 +7,17 @@ defmodule Yodado.DefinitionTest do
     alias Yodado.Definition.All, as: All
 
     test "is true if all conditions match" do
-      definition = All[conditions: [true, true]]
+      definition = All[definitions: [true, true]]
       assert Rule.matches?(definition, [])
     end
 
     test "is false unless all conditions match" do
-      definition = All[conditions: [true, false]]
+      definition = All[definitions: [true, false]]
       assert !Rule.matches?(definition, [])
     end
 
     test "it passes the state through to child conditions" do
-      definition = All[conditions: [
+      definition = All[definitions: [
         fn(state) -> state[:foo] == 1 end,
         fn(state) -> state[:bar] == 2 end
       ]]
@@ -26,7 +26,7 @@ defmodule Yodado.DefinitionTest do
     end
 
     test "it generates JSON" do
-      definition = All[conditions: [true, false]]
+      definition = All[definitions: [true, false]]
       expected_json = "{\"operand\":\"all\",\"conditions\":[{\"operand\":\"boolean\",\"value\":true},{\"operand\":\"boolean\",\"value\":false}]}"
       actual_json = Rule.json(definition) |> JSEX.encode!
       assert actual_json == expected_json
@@ -38,17 +38,17 @@ defmodule Yodado.DefinitionTest do
     alias Yodado.Definition.Any, as: Any
 
     test "is true if any conditions match" do
-      definition = Any[conditions: [false, true]]
+      definition = Any[definitions: [false, true]]
       assert Rule.matches?(definition, [])
     end
 
     test "is false unless any conditions match" do
-      definition = Any[conditions: [false, false]]
+      definition = Any[definitions: [false, false]]
       assert !Rule.matches?(definition, [])
     end
 
     test "it passes the state through to child conditions" do
-      definition = Any[conditions: [
+      definition = Any[definitions: [
         fn(state) -> state[:foo] == 1 end,
         fn(state) -> state[:bar] == 2 end
       ]]
@@ -57,7 +57,7 @@ defmodule Yodado.DefinitionTest do
     end
 
     test "it generates JSON" do
-      definition = Any[conditions: [true, false]]
+      definition = Any[definitions: [true, false]]
       expected_json = "{\"operand\":\"any\",\"conditions\":[{\"operand\":\"boolean\",\"value\":true},{\"operand\":\"boolean\",\"value\":false}]}"
       actual_json = Rule.json(definition) |> JSEX.encode!
       assert actual_json == expected_json
@@ -69,17 +69,17 @@ defmodule Yodado.DefinitionTest do
     alias Yodado.Definition.IncludedIn, as: IncludedIn
 
     test "is true if the value of the param in the state is in the list of values" do
-      definition = IncludedIn[param_name: "username", value: ["francis", "bill", "zoey", "louis"]]
+      definition = IncludedIn[actual_state_param_name: "username", allowed_values: ["francis", "bill", "zoey", "louis"]]
       assert Rule.matches?(definition, [{"username", "bill"}])
     end
 
     test "is false if the value of the param in the state is not in the list of values" do
-      definition = IncludedIn[param_name: "username", value: ["francis", "bill", "zoey", "louis"]]
+      definition = IncludedIn[actual_state_param_name: "username", allowed_values: ["francis", "bill", "zoey", "louis"]]
       assert !Rule.matches?(definition, [{"username", "coach"}])
     end
 
     test "it generates JSON" do
-      definition = IncludedIn[param_name: "username", value: ["francis", "bill", "zoey", "louis"]]
+      definition = IncludedIn[actual_state_param_name: "username", allowed_values: ["francis", "bill", "zoey", "louis"]]
       expected_json = "{\"operand\":\"included_in\",\"param_name\":\"username\",\"value\":[\"francis\",\"bill\",\"zoey\",\"louis\"]}"
       actual_json = Rule.json(definition) |> JSEX.encode!
       assert actual_json == expected_json
@@ -92,17 +92,17 @@ defmodule Yodado.DefinitionTest do
     alias Yodado.Definition.Is, as: Is
 
     test "is true if the value of the param in the state equals the value" do
-      definition = Is[param_name: "session_on", value: "yes"]
+      definition = Is[actual_state_param_name: "session_on", allowed_value: "yes"]
       assert Rule.matches?(definition, [{"session_on", "yes"}]) 
     end
 
     test "is false if the value of the param in the state does not equals the value" do
-      definition = Is[param_name: "session_on", value: "yes"]
+      definition = Is[actual_state_param_name: "session_on", allowed_value: "yes"]
       assert !Rule.matches?(definition, [{"session_on", "HAHA"}]) 
     end
 
     test "it generates JSON" do
-      definition = Is[param_name: "session_on", value: "yes"]
+      definition = Is[actual_state_param_name: "session_on", allowed_value: "yes"]
       expected_json = "{\"operand\":\"is\",\"param_name\":\"session_on\",\"value\":\"yes\"}"
       actual_json = Rule.json(definition) |> JSEX.encode!
       assert actual_json == expected_json
@@ -152,13 +152,13 @@ defmodule Yodado.DefinitionTest do
     """
     actual_definition = json |> JSEX.decode!(labels: :atom) |> Yodado.Definition.from_json
     expected_definition = 
-      Yodado.Definition.Any[conditions: [
-        Yodado.Definition.All[conditions: [
-          Yodado.Definition.IncludedIn[param_name: "username", value: ["madlep", "gstamp"]],
-          Yodado.Definition.IncludedIn[param_name: "host", value: ["themeforest.net", "codecanyon.net"]]
+      Yodado.Definition.Any[definitions: [
+        Yodado.Definition.All[definitions: [
+          Yodado.Definition.IncludedIn[actual_state_param_name: "username", allowed_values: ["madlep", "gstamp"]],
+          Yodado.Definition.IncludedIn[actual_state_param_name: "host", allowed_values: ["themeforest.net", "codecanyon.net"]]
         ]],
-        Yodado.Definition.Is[param_name: "session_on"],
-        Yodado.Definition.IncludedIn[param_name: "role", value: ["developer"]]
+        Yodado.Definition.Is[actual_state_param_name: "session_on"],
+        Yodado.Definition.IncludedIn[actual_state_param_name: "role", allowed_values: ["developer"]]
       ]]
 
     assert actual_definition == expected_definition
