a64755b2a86b7896ff21fb1282c64378639989aa
Pete Johns <paj-github@johnsy.com>
2014-03-19 10:55:48 +1100

    Forgot arity

--- test/yodado/feature_set_test.exs
+++ test/yodado/feature_set_test.exs
@@ -3,7 +3,7 @@ defmodule Yodado.FeatureSetTest do
     use ExUnit.Case, async: true
     alias Yodado.FeatureSet, as: FeatureSet
 
-    test "multi_do returns a keyword list of features" do
+    test "multi_do/1 returns a keyword list of features" do
       result = FeatureSet.multi_do(nil)
       assert Keyword.keyword?(result)
     end
