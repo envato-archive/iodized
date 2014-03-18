1eff253b991787b8ec042e749b4e4dfed4b451e5
Pete Johns <paj-github@johnsy.com>
2014-03-19 10:14:55 +1100

    Start adding tests for Yodado.FeatureSetTest

---
+++ test/yodado/feature_set_test.exs
@@ -0,0 +1,11 @@
+defmodule Yodado.FeatureSetTest do
+  defmodule AllTest do
+    use ExUnit.Case, async: true
+    alias Yodado.FeatureSet, as: FeatureSet
+
+    test "multi_do returns a keyword list of features" do
+      result = FeatureSet.multi_do(nil)
+      assert Keyword.keyword?(result)
+    end
+  end
+end
