953c1fc1c1d32e2d7d30d6fbb8c801650e4d239c
Pete Johns <paj-github@johnsy.com>
2014-03-19 16:12:21 +1100

    DRY-up specs

--- test/yodado/feature_test.exs
+++ test/yodado/feature_test.exs
@@ -3,6 +3,31 @@ defmodule Yodado.FeatureTest do
     use ExUnit.Case, async: true
     alias Yodado.Feature, as: Feature
 
+    setup do
+      {:ok, [
+          feature: Feature.Feature[title: "Fancy feature",
+            description: "Does stuff",
+            master_switch_state: nil,
+            definition: Yodado.Definition.All[definitions: [
+                Yodado.Definition.Is[actual_state_param_name: "username", allowed_value: "paj"]
+              ]
+            ]
+          ],
+          serialized_feature: [
+            title: "Fancy feature",
+            description: "Does stuff",
+            master_switch_state: nil,
+            definition: [
+              operand: "all",
+              definitions: [
+                [operand: "is", param_name: "username", value: "paj"]
+              ]
+            ]
+          ]
+        ]
+      }
+    end
+
     test "do?/2 returns a tuple {:ok, boolean} " do
       fancy_feature = Feature.Feature[title: "Fancy feature",
         description: "Does stuff",
@@ -15,55 +40,16 @@ defmodule Yodado.FeatureTest do
       assert elem(result, 1) |> is_boolean
     end
 
-    test "serialization" do
-
-      feature = Feature.Feature[title: "Fancy feature",
-        description: "Does stuff",
-        master_switch_state: nil,
-        definition: Yodado.Definition.All[definitions: [
-            Yodado.Definition.Is[actual_state_param_name: "username",
-              allowed_value: "paj"]
-          ]
-        ]
-      ]
-
-      serialized_feature = [
-        title: "Fancy feature",
-        description: "Does stuff",
-        master_switch_state: nil,
-        definition: [
-          operand: "all",
-          definitions: [
-            [operand: "is", param_name: "username", value: "paj"]
-          ]
-        ]
-      ]
+    test "serialization", context do
+      feature = context[:feature]
+      serialized_feature = context[:serialized_feature]
 
       assert(Feature.json(feature) === serialized_feature)
     end
 
-    test "deserialization" do
-      feature = Feature.Feature[title: "Fancy feature",
-        description: "Does stuff",
-        master_switch_state: nil,
-        definition: Yodado.Definition.All[definitions: [
-            Yodado.Definition.Is[actual_state_param_name: "username",
-              allowed_value: "paj"]
-          ]
-        ]
-      ]
-
-      serialized_feature = [
-        title: "Fancy feature",
-        description: "Does stuff",
-        master_switch_state: nil,
-        definition: [
-          operand: "all",
-          definitions: [
-            [operand: "is", param_name: "username", value: "paj"]
-          ]
-        ]
-      ]
+    test "deserialization", context do
+      feature = context[:feature]
+      serialized_feature = context[:serialized_feature]
 
       assert(Feature.from_json(serialized_feature) == feature)
     end
