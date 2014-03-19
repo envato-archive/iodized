38b1a88b885281415a1bf25f4247bc8f23190143
Julian Doherty (madlep) <madlep@madlep.com>
2014-03-19 17:22:19 +1100

    generated erlang thrift source

---
+++ src/feature_types.hrl
@@ -0,0 +1,4 @@
+-ifndef(_feature_types_included).
+-define(_feature_types_included, yeah).
+
+-endif.
---
+++ src/features_thrift.hrl
@@ -0,0 +1,5 @@
+-ifndef(_features_included).
+-define(_features_included, yeah).
+-include("feature_types.hrl").
+
+-endif.
