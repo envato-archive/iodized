e063f3c9c20c921f3706164514868afd46e7cc8b
Pete Johns <paj-github@johnsy.com>
2014-03-19 21:11:22 +1100

    Fix up feature_set

--- lib/yodado/feature_set.ex
+++ lib/yodado/feature_set.ex
@@ -3,8 +3,11 @@ defmodule Yodado.FeatureSet do
 
   def multi_do(params) do
     {:ok, features} = @persistence.all()
+
     lc feature inlist features do
-      {binary_to_atom(feature.title), true}
+      {:ok, state} = Yodado.Feature.do?(feature, params)
+      {feature.title, state}
     end
+
   end
 end
--- test/yodado/feature_set_test.exs
+++ test/yodado/feature_set_test.exs
@@ -3,9 +3,16 @@ defmodule Yodado.FeatureSetTest do
     use ExUnit.Case, async: true
     alias Yodado.FeatureSet, as: FeatureSet
 
-    test "multi_do/1 returns a keyword list of features" do
-      result = FeatureSet.multi_do(nil)
-      assert Keyword.keyword?(result)
+    test "multi_do/1 returns a list of 2-tuples {feature.title, state}" do
+      # TODO Inject fake data
+      result = FeatureSet.multi_do([])
+      assert is_list(result)
+
+      [first | _ ] = result
+      {title, state} = first
+
+      assert is_binary(title)
+      assert is_boolean(state)
     end
   end
 end
