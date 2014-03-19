eb1778c958ef2d4be4774c95b43f6bdf46f37617
Pete Johns <paj-github@johnsy.com>
2014-03-19 14:47:06 +1100

    Get feature list from persistence layer

--- lib/yodado/feature_set.ex
+++ lib/yodado/feature_set.ex
@@ -1,5 +1,10 @@
 defmodule Yodado.FeatureSet do
+  @persistence Yodado.FeaturePersistence.Mnesia
+
   def multi_do(params) do
-    ["foo": true, "bar": false]
+    {:ok, features} = @persistence.all()
+    lc feature inlist features do
+      {binary_to_atom(feature.title), true}
+    end
   end
 end
