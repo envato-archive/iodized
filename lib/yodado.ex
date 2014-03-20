7c5fa54963ff2290997b2ef44b0e5c6e677e02da
Julian Doherty (madlep) <madlep@madlep.com>
2014-03-20 16:47:21 +1100

    hax thrift client to test connecting to server

--- lib/yodado.ex
+++ lib/yodado.ex
@@ -19,6 +19,21 @@ defmodule Yodado do
       port: 12345)
   end
 
+  # debugging only, kill this shortly
+  def test_thrift() do
+    {:ok, c} = :thrift_client_util.new('localhost', 12345, :features_thrift, [])
+    {c, {:ok, pong}} = :thrift_client.call(c, :ping, [])
+    IO.puts "ping=#{pong}"
+
+    args = :dict.from_list([{"ip", "1.2.3.4"}, {"username", "madlep"}])
+    {c,{:ok, features_dict}} = :thrift_client.call(c, :feature_set, [args])
+    features = :dict.to_list(features_dict)
+    IO.puts "feature_set=#{inspect(features)}"
+
+    {c, :ok} = :thrift_client.close(c) 
+    :ok
+  end
+
   defp start_mnesia() do
    {:ok, data_dir} = :application.get_env(:yodado, :data_dir)
    :application.set_env(:mnesia, :dir, data_dir)
