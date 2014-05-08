644f0d0cca08a89213102525e1a820fa09e4a810
Julian Doherty <madlep@madlep.com>
2014-05-08 23:39:30 +1000

    WIP on admin UI

--- priv/static/js/iodized.js
+++ priv/static/js/iodized.js
@@ -2,23 +2,44 @@
 var FeatureBox = React.createClass({
 
   getInitialState: function() {
-    return {data: [ {
-      "title": "enable_foo",
-      "description": "I like to foo",
-      "master_switch_state": null
-    },
-    {
-      "title": "activate_bar",
-      "description": "DEPLOY THE BAR!",
-      "master_switch_state": true
-    }]};
+    return {data: [], selectedFeature: null};
+  },
+
+  fetchFeatures: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(status, err);
+      }
+    });
+  },
+
+  componentWillMount: function() {
+    this.fetchFeatures();
+    setInterval(this.fetchFeatures, this.props.pollInterval);
+  },
+
+  onEdit: function(feature) {
+    var newState = $.extend({}, this.state, {selectedFeature: feature});
+    this.setState(newState);
+  },
+
+  onStopEdit: function() {
+    var newState = $.extend({}, this.state, {selectedFeature: null});
+    this.setState(newState);
   },
 
   render: function() {
+
     return (
-      <div className="featureBox">
+      <div className="featureBox row">
         <h2>Features</h2>
-        <FeatureList data={this.state.data} />
+        <FeatureList data={this.state.data} onEdit={this.onEdit} />
+        <FeatureForm feature={this.state.selectedFeature} onStopEdit={this.onStopEdit}/>
       </div>
     );
   }
@@ -26,8 +47,9 @@ var FeatureBox = React.createClass({
 
 var FeatureList = React.createClass({
   render: function() {
+    var self = this;
     var featureNodes = this.props.data.map(function (feature, index) {
-      return <Feature key={index} title={feature.title} description={feature.description} master_switch_state={feature.master_switch_state}/>
+      return <Feature key={index} feature={feature} onEdit={self.props.onEdit}/>
     });
     return (
       <div className="featureList">{featureNodes}</div>
@@ -36,9 +58,15 @@ var FeatureList = React.createClass({
 });
 
 var Feature = React.createClass({
+  handleEdit: function() {
+    this.props.onEdit(this.props.feature);
+    return false;
+  },
+
   render: function() {
+    var feature = this.props.feature;
     var switch_state = null;
-    switch(this.props.master_switch_state) {
+    switch(feature.master_switch_state) {
       case true:
         switch_state = "Override on";
         break;
@@ -51,17 +79,56 @@ var Feature = React.createClass({
     }
 
     return(
-      <div className="feature">
-        <h3 className="featureTitle">{this.props.title}</h3>
-        <div class="featureDescription">{this.props.description}</div>
-        <div class="featureMasterSwitchState">{switch_state}</div>
+      <div className="feature panel">
+        <div className="row">
+          <div className="featureTitle columns"><h3>{feature.title}</h3></div>
+        </div>
+        <div className="row">
+          <div className="featureDescription small-9 columns">{feature.description}</div>
+          <div className="featureMasterSwitchState small-1 columns"> {switch_state}</div>
+          <div className="editFeature small-1 columns"><a href="#" onClick={this.handleEdit}>edit</a></div>
+          <div className="deleteFeature small-1 columns">delete</div>
+        </div>
       </div>
     )
   }
 });
 
+var FeatureForm = React.createClass({
+  componentDidMount: function() {
+    self = this;
+    $("#featureEditModal").
+      foundation("reveal");
+
+    $(document).
+      on("closed", "[data-reveal]", function() {
+        self.props.onStopEdit();
+      });
+  },
+
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.props.feature) {
+      $("#featureEditModal").foundation("reveal", "open");
+    } else {
+      $("#featureEditModal").foundation("reveal", "close");
+    }
+  },
+
+  render: function() {
+    var feature = {};
+    if (this.props.feature) {
+      feature = this.props.feature;
+    }
+    return (
+      <div id="featureEditModal" className="reveal-modal" data-reveal="true">
+        <h2>{feature.title}</h2>
+      </div>
+    );
+  }
+});
+
 React.renderComponent(
-  <FeatureBox />,
+  <FeatureBox url="/admin/api/features" pollInterval={2000}/>,
   document.getElementById('container')
 );
 
