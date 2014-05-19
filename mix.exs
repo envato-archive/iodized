2db518dfffaa719a0a69c3b4fd8f897b3d750882
Julian Doherty <madlep@madlep.com>
2014-05-19 15:37:18 +1000

    add uuid

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Iodized.Mixfile do
     [
       {:cowboy, github: "extend/cowboy", tag: "0.9.0"},
       {:jsex, github: "talentdeficit/jsex", tag: "v2.0.0"},
-      {:thrift, github: "envato/thrift-erlang", tag: "0.9.1.envato"} # TODO need nicer way of managing thrift
+      {:thrift, github: "envato/thrift-erlang", tag: "0.9.1.envato"}, # TODO need nicer way of managing thrift
+      {:uuid, github: "avtobiff/erlang-uuid", tag: "v0.4.6"}
     ]
   end
 end
