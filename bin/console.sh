cd024b2417c044a432d3aebed1ca8427119d014d
Julian Doherty <madlep@madlep.com>
2014-05-27 15:40:31 +1000

    remove name config till I get it working properly

--- bin/console.sh
+++ bin/console.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")/.."
-elixir -S mix run --no-halt
+iex -S mix
--- bin/start_console.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-HOSTNAME=`hostname -f`
-APP=iodized
-COOKIE=$APP@$HOSTNAME
-iex --name $COOKIE -S mix
