260cbdf8123ab563412cb3ef09f07395c6f5d286
Damir Kotoric <damir.kotoric@envato.com>
2014-06-13 10:51:52 +1000

    created add feature toggle functionality

--- priv/static/js/main.js
+++ priv/static/js/main.js
@@ -1,10 +1,3 @@
-// $('.js-switch').each(
-//   function () {
-//     var switchery = new Switchery($(this));
-//   }
-// );
-
-
 $( document ).ready(function() {
   // Set up switchery
   var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
@@ -12,7 +5,7 @@ $( document ).ready(function() {
     var switchery = new Switchery(elem,
       {
         color          : '#2e507f',
-        secondaryColor : '#dfdfdf',
+        secondaryColor : '#d5dce5',
         className      : 'switchery',
         disabled       : false,
         disabledOpacity: 0.5,
@@ -23,5 +16,27 @@ $( document ).ready(function() {
       $elem = $(elem);
       $elem.closest('.feature--on, .feature--off').toggleClass('feature--on feature--off');
     };
+  });
+
+  // Collapse / expand feature
+  $('.feature--on > .feature__teaser, .feature--off > .feature__teaser').on('click', function (event) {
+    event.preventDefault();
+    $(this).closest('.feature--on, .feature--off').toggleClass('is-expanded is-collapsed');
+  });
+
+  // New feature
+  $('.new-feature__add').click(function (event) {
+    event.preventDefault();
+    var newFeature = $(this).closest('.new-feature')
+    if (newFeature.is(".is-expanded, .is-collapsed")) {
+      newFeature.toggleClass('is-expanded is-collapsed');
+    } else {
+      newFeature.addClass('is-expanded');
+    }
+  });
+  $('.new-feature__close').click(function (event) {
+    event.preventDefault();
+    var newFeature = $(this).closest('.new-feature')
+    newFeature.toggleClass('is-expanded is-collapsed');
   });
 });
