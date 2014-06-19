390314f1316b4f5af3e633368887161296848b4c
Rowan Sloan <rnsloan1@gmail.com>
2014-06-19 13:12:37 +1000

    create iodized namespace object. Split logic into methods

--- priv/static/js/main.js
+++ priv/static/js/main.js
@@ -1,49 +1,66 @@
-$( document ).ready(function() {
-  // Set up switchery
-  var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-  elems.forEach(function(elem) {
-    var switchery = new Switchery(elem,
-      {
-        color          : '#2e507f',
-        secondaryColor : '#d5dce5',
-        className      : 'switchery',
-        disabled       : false,
-        disabledOpacity: 0.5,
-        speed          : '0.3s'
-      }
-    );
-    elem.onchange = function() {
-      $elem = $(elem);
-      $elem.closest('.feature--on, .feature--off').toggleClass('feature--on feature--off');
-    };
-  });
+var iodized = {};
 
-  // Collapse / expand feature
-  $('.feature__view-edit-button').on('click', function (event) {
-    event.preventDefault();
-    $(this).closest('.feature--on, .feature--off').toggleClass('is-expanded is-collapsed');
-  });
+iodized.init = function() {
+    iodized.switchery();
+    iodized.addNewFeature();
+    iodized.changeFeatureSettingsVisibility();
+}
 
-  // New feature
-  $('.new-feature__add').click(function (event) {
-    event.preventDefault();
-    var newFeature = $(this).closest('.new-feature')
-    if (newFeature.is(".is-expanded, .is-collapsed")) {
-      newFeature.toggleClass('is-expanded is-collapsed');
-      newFeature.find("input:first-of-type").focus();
-    } else {
-      newFeature.addClass('is-expanded');
-      newFeature.find("input:first-of-type").focus();
-    }
-  });
-  $('.new-feature__close').click(function (event) {
-    event.preventDefault();
-    var newFeature = $(this).closest('.new-feature')
-    newFeature.toggleClass('is-expanded is-collapsed');
-  });
-  $('.new-feature__submit').click(function (event) {
-    event.preventDefault();
-    var newFeature = $(this).closest('.new-feature')
-    newFeature.toggleClass('is-expanded is-collapsed');
-  });
+iodized.switchery = function() {
+    var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+    elems.forEach(function(elem) {
+        var switchery = new Switchery(elem,
+            {
+                color          : '#2e507f',
+                secondaryColor : '#d5dce5',
+                className      : 'switchery',
+                disabled       : false,
+                disabledOpacity: 0.5,
+                speed          : '0.3s'
+            }
+        );
+        elem.onchange = function() {
+            $elem = $(elem);
+            $elem.closest('.feature--on, .feature--off').toggleClass('feature--on feature--off');
+        };
+    });
+};
+
+iodized.addNewFeature = function() {
+    $('.new-feature__add').click(function (event) {
+        event.preventDefault();
+        var newFeature = $(this).closest('.new-feature')
+        if (newFeature.is(".is-expanded, .is-collapsed")) {
+            newFeature.toggleClass('is-expanded is-collapsed');
+            newFeature.find("input:first-of-type").focus();
+        } else {
+            newFeature.addClass('is-expanded');
+            newFeature.find("input:first-of-type").focus();
+        }
+    });
+
+    $('.new-feature__close').click(function (event) {
+        event.preventDefault();
+        var newFeature = $(this).closest('.new-feature')
+        newFeature.toggleClass('is-expanded is-collapsed');
+    });
+
+    $('.new-feature__submit').click(function (event) {
+        event.preventDefault();
+        var newFeature = $(this).closest('.new-feature')
+        newFeature.toggleClass('is-expanded is-collapsed');
+    });
+}
+
+iodized.changeFeatureSettingsVisibility = function() {
+    $('.feature__view-edit-button').on('click', function (event) {
+        event.preventDefault();
+        $(this).closest('.feature--on, .feature--off').toggleClass('is-expanded is-collapsed');
+    });
+}
+
+
+$(document).ready(function() {
+    iodized.init();
 });
+
