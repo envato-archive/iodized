0b766ae3ee8ca7080259122a6ce536a73e3e18ed
Julian Doherty <madlep@madlep.com>
2014-06-20 14:55:31 +1000

    make updating of features work

--- priv/static/js/iodized.js
+++ priv/static/js/iodized.js
@@ -41,9 +41,7 @@ var FeatureBox = React.createClass({
   },
 
   updateFeature: function(feature) {
-      console.log('update');
-      return false;
-    //this.props.featureRepo.updateFeature(feature, this.refresh);
+    this.props.featureRepo.updateFeature(feature, this.refresh);
   },
 
   render: function() {
@@ -68,7 +66,7 @@ var FeatureList = React.createClass({
     var self = this;
     var featureNodes = this.props.features.map(function (feature, index) {
       return (
-        <Feature key={index} feature={feature} editFeature={this.props.editFeature} deleteFeature={this.props.deleteFeature}/>
+        <Feature key={index} feature={feature} editFeature={this.props.editFeature} deleteFeature={this.props.deleteFeature} updateFeature={this.props.updateFeature}/>
       )
     }.bind(this));
     return (
@@ -78,43 +76,73 @@ var FeatureList = React.createClass({
 });
 
 var Feature = React.createClass({
+  getInitialState: function() {
+    var editingFeature = $.extend({}, this.props.feature);
+    return {editingFeature: editingFeature, expanded: false};
+  },
+
   switchState: function (element) {
       if (element === 'checkbox') {
-          switch (this.props.feature.master_switch_state) {
+          switch (this.state.editingFeature.master_switch_state) {
               case "on":
               case "dynamic":
-                  return 'yes'
+                  return true;
+              case "off":
+                  return false;
           }
       }
       else {
-          switch (this.props.feature.master_switch_state) {
+          var expanded_class = this.state.expanded ? "is-expanded" : "is-collapsed";
+          switch (this.state.editingFeature.master_switch_state) {
               case "on":
               case "dynamic":
-                  return 'feature--on is-collapsed'
+                  return 'feature--on ' + expanded_class;
               case "off":
-                  return 'feature--off is-collapsed'
+                  return 'feature--off ' + expanded_class;
           }
       }
   },
 
   handleEdit: function(){
-    $(this.getDOMNode()).closest('.feature--on, .feature--off').toggleClass('is-expanded is-collapsed')
-    false;
+    this.setState({expanded: !this.state.expanded})
+    return false;
+  },
+
+  handleToggle: function(e){
+    var feature = this.state.editingFeature;
+    if (e.target.checked) {
+      feature.master_switch_state = feature.definition == null ? "on" : "dynamic";
+    } else {
+      feature.master_switch_state = "off";
+    }
+    this.setState({editingFeature: feature, exanded: false});
+    this.props.updateFeature(this.state.editingFeature);
+    return false;
   },
 
   handleUpdate: function () {
-    this.props.updateFeature(this.props.feature);
+    this.props.updateFeature(this.state.editingFeature);
     this.handleEdit();
-    false;
+    return false;
   },
 
   handleDelete: function(){
-    this.props.deleteFeature(this.props.feature);
+    this.props.deleteFeature(this.state.editingFeature);
     false;
   },
 
+  handleChange: function() {
+    var feature = this.state.editingFeature;
+    feature.title = this.refs.title.getDOMNode().value;
+    feature.description = this.refs.description.getDOMNode().value;
+    feature.master_switch_state = this.refs.master_switch_state.getDOMNode().value;
+    feature.definition = null;
+    this.setState({editingFeature: feature});
+    return true;
+  },
+
   render: function() {
-    var feature = this.props.feature;
+    var feature = this.state.editingFeature;
     return(
       <div className={this.switchState('feature')}>
           <div className="feature__view">
@@ -124,24 +152,24 @@ var Feature = React.createClass({
                   <p>{feature.description}</p>
               </div>
               <div className="feature__switch">
-                  <input type="checkbox" defaultChecked={this.switchState('checkbox')} className="js-switch" />
+                  <input type="checkbox" checked={this.switchState('checkbox')} className="js-switch" onChange={this.handleToggle}/>
               </div>
           </div>
           <div className="feature__edit">
               <form className="featureEditForm" ref="form" role="form">
                   <div className="form-group">
                       <label className="control-label" htmlFor="featureTitleInput">Feature Name*</label>
-                      <input type="text" className="form-control input-lg" ref="title" id="featureTitleInput" defaultValue={feature.title} />
+                      <input type="text" className="form-control input-lg" ref="title" id="featureTitleInput" value={feature.title} onChange={this.handleChange}/>
                       <small>Lower case and underscores only, no spaces</small>
                   </div>
                   <div className="form-group">
                       <label className="control-label" htmlFor="featureDescriptionInput">Description</label>
-                      <textarea className="form-control input-lg" ref="description" rows="3" id="featureDescriptionInput" defaultValue={feature.description}></textarea>
+                      <textarea className="form-control input-lg" ref="description" rows="3" id="featureDescriptionInput" value={feature.description} onChange={this.handleChange}></textarea>
                       <small>A word or two on what the feature does so it can be easily identified if there are many active feature toggles on the page</small>
                   </div>
                   <div className="form-group">
                       <label htmlFor="featureMasterSwitchStateInput">Master Switch</label>
-                      <select id="featureMasterSwitchStateInput" value={feature.master_switch_state} className="form-control" ref="master_switch_state">
+                      <select id="featureMasterSwitchStateInput" value={feature.master_switch_state} className="form-control" ref="master_switch_state" onChange={this.handleChange}>
                           <option value="dynamic">Dynamic</option>
                           <option value="on">On</option>
                           <option value="off">Off</option>
@@ -306,8 +334,8 @@ FeatureRepo.prototype.updateFeature = function(feature, onSuccess, onError){
     contentType: 'application/json',
     type: 'PUT',
     data: JSON.stringify(feature),
-    success: onSuccess,
     error: onError || function(xhr, status, err) {
+    success: onSuccess,
       console.log(status, err);
     }
   });
