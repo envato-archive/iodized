5ee8f9343967b4707a3e64822641598f04377630
Matthew Doyle <matthew.doyle@envato.com>
2014-09-22 20:18:51 +1000

    Add (kinda) working prototype of child definitions
    - Add is working, remove, not so much.
    - Recursion, see recursion.

--- priv/ui/app/js/react/feature/settings/definition.jsx
+++ priv/ui/app/js/react/feature/settings/definition.jsx
@@ -6,6 +6,16 @@ var Definition = React.createClass({
     return {children: []};
   },
 
+  handleAddBtn: function (event) {
+    console.log("here", this.state.children);
+    var children = this.state.children;
+    this.setState({children: children.concat([children.length + 1])});
+  },
+
+  handleRemoveBtn: function (event) {
+    this.setState({children: []});
+  },
+
   render: function() {
     return (
       <li className="list-group-item">
@@ -26,12 +36,12 @@ var Definition = React.createClass({
             <label className="sr-only" htmlFor="">Free text</label>
             <input type="email" className="form-control" id="" placeholder="Free Text" size="40" />
           </div>
-          <a href="#"><span className="glyphicon glyphicon-minus-sign feature__setting-icon  pull-right"></span></a>
-          <a href="#"><span className="glyphicon glyphicon-plus-sign feature__setting-icon pull-right"></span></a>
+          <a onClick={this.handleRemoveBtn}><span className="glyphicon glyphicon-minus-sign feature__setting-icon  pull-right"></span></a>
+          <a onClick={this.handleAddBtn}><span className="glyphicon glyphicon-plus-sign feature__setting-icon pull-right"></span></a>
         </div>
-        {this.state.children.map(function(children, i) {
+        {this.state.children.map(function(i) {
           return (
-            <Definition />
+            <Definition id={i} />
           );
         }, this)}
       </li>
