132363d71c975a92d32e92d0997e5579fbd6f71e
Matthew Doyle <matthew.doyle@envato.com>
2014-09-22 20:03:36 +1000

    Add working prototype for `add new` and `clear all`

--- priv/ui/app/js/react/feature/settings.jsx
+++ priv/ui/app/js/react/feature/settings.jsx
@@ -1,13 +1,19 @@
 var React = require("react/addons");
+var Definition = require("./settings/definition.jsx");
 
 var FeatureSettings = React.createClass({
 
+  getInitialState: function() {
+    return {definitions: []};
+  },
+
   handleAddNewBtn: function (event) {
-    console.log("add", event);
+    var defs = this.state.definitions;
+    this.setState({definitions: defs.concat([defs.length + 1])});
   },
 
   handleClearAllBtn: function (event) {
-    console.log("clear", event);
+    this.setState({definitions: []});
   },
 
   render: function() {
@@ -25,33 +31,18 @@ var FeatureSettings = React.createClass({
             </div>
             <label> of the following is true:</label>
             <div className="pull-right">
-              <button type="button" className="btn btn-default" onClick={this.handleAddNewBtn(this)}>Add New</button>
-              <button type="button" className="btn btn-default" onClick={this.handleClearAllBtn(this)}>Clear All</button>
+              <button type="button" className="btn btn-default" onClick={this.handleAddNewBtn}>Add New</button>
+              <button type="button" className="btn btn-default" onClick={this.handleClearAllBtn}>Clear All</button>
             </div>
           </div>
         </li>
-        <li className="list-group-item">
-          <div className="form-inline" role="form">
-            <div className="form-group">
-              <label className="sr-only" htmlFor="">Attributes</label>
-              <input type="email" className="form-control" id="" placeholder="Main Attributes" />
-            </div>
-            <div className="form-group">
-              <label className="sr-only" htmlFor="">Condition</label>
-              <select className="form-control">
-                <option value="includeIn">include in</option>
-                <option value="is">is</option>
-                <option value="percentage">percentage</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <label className="sr-only" htmlFor="">Free text</label>
-              <input type="email" className="form-control" id="" placeholder="Free Text" size="40" />
-            </div>
-            <a href="#"><span className="glyphicon glyphicon-minus-sign feature__setting-icon  pull-right"></span></a>
-            <a href="#"><span className="glyphicon glyphicon-plus-sign feature__setting-icon pull-right"></span></a>
-          </div>
-        </li>
+        
+        {this.state.definitions.map(function(i) {
+          return (
+            <Definition id={i}/>
+          );
+        }, this)}
+
       </ul>
     );
   }
