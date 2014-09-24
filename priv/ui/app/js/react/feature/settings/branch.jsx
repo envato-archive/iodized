5dbb1e06addec5f66987ae36f0b922731a87970a
Matthew Doyle <matthew.doyle@envato.com>
2014-09-24 12:52:10 +1000

    Item removal improvements

--- priv/ui/app/js/react/feature/settings/branch.jsx
+++ priv/ui/app/js/react/feature/settings/branch.jsx
@@ -15,7 +15,9 @@ var SettingsBranch = React.createClass({
   handleAddBtn: function (event) {
     var definitions = this.state.definitions;
     this.setState({definitions: definitions.concat([{
-        operand: 'is'
+        operand: 'is',
+        param_name: '',
+        value: ''
       }]
     )});
   },
@@ -27,53 +29,52 @@ var SettingsBranch = React.createClass({
   handleAddParent: function (event) {
     var definitions = this.state.definitions;
     this.setState({definitions: definitions.concat([{
-        operand: 'any'
+        operand: 'any',
+        definitions: []
       }]
     )});
   },
   handleConditionChange: function (event) {
   },
 
-  handleRemoveChild: function (i) {
+  handleRemoveChild: function (i, key) {
     var definitions = this.state.definitions;
-    // TODO make this work
-    console.log("remove at index", i, definitions[i].operand);
-    var definitionsClone = definitions.slice(0);
-    definitionsClone.splice(i, 1);
-    this.setState({definitions: definitionsClone});
+    console.log("remove at index", i, key);
+
+    definitions.splice(i, 1);
+    this.forceUpdate();
   },
 
   render: function() {
     
     return (
-      <div className="form-inline" role="form">
-        <label>If </label>
-        <label className="sr-only" htmlFor="">Condition</label>
-        <select value={this.state.definition.operand} className="form-control feature-settings__condition" onChange={this.handleConditionChange}>
-          <option value="all">all</option>
-          <option value="any">any</option>
-        </select>
-        <label>of the following is true:</label>
-        <a onClick={this.handleRemoveBtn}><span className="glyphicon glyphicon-minus-sign feature__setting-icon  pull-right"></span></a>
-        <a onClick={this.handleAddBtn}><span className="glyphicon glyphicon-plus-sign feature__setting-icon pull-right"></span></a>
-        <a onClick={this.handleAddParent}><span className="glyphicon glyphicon-download feature__setting-icon pull-right"></span></a>
+      <div className="list-group-item feature-settings__child-node">
+        <div className="form-inline" role="form">
+          <label>If </label>
+          <label className="sr-only" htmlFor="">Condition</label>
+          <select value={this.state.definition.operand} className="form-control feature-settings__condition" onChange={this.handleConditionChange}>
+            <option value="all">all</option>
+            <option value="any">any</option>
+            <option value="none">none</option>
+          </select>
+          <label>of the following is true:</label>
+          <a onClick={this.handleRemoveBtn}><span className="glyphicon glyphicon-minus-sign feature__setting-icon  pull-right"></span></a>
+          <a onClick={this.handleAddBtn}><span className="glyphicon glyphicon-plus-sign feature__setting-icon pull-right"></span></a>
+          <a onClick={this.handleAddParent}><span className="glyphicon glyphicon-download feature__setting-icon pull-right"></span></a>
 
-        {this.state.definitions.map(function(definition, i) {
-          var node;
-          console.log("operand", definition.operand, "definition index:", i);
+          {this.state.definitions.map(function(definition, i) {
+            var node;
+            var key = definition.operand + i + Math.random();
 
-          if (definition.operand === "any" || definition.operand === "all" || definition.operand === "none") {
-            node = <SettingsBranch definition={definition} />;
-          } else {
-            node = <SettingsNode definition={definition} removeHandler={this.handleRemoveChild.bind(this, i)}/>;
-          }
+            if (definition.operand === "any" || definition.operand === "all" || definition.operand === "none") {
+              node = <SettingsBranch key={key} definition={definition} />;
+            } else {
+              node = <SettingsNode key={key} definition={definition} removeHandler={this.handleRemoveChild.bind(this, i, key)}/>;
+            }
 
-          return (
-            <div className="list-group-item feature-settings__child-node">
-              { node }
-            </div>
-          );
-        }, this)}
+            return ( node );
+          }, this)}
+        </div>
       </div>
     );
   }
