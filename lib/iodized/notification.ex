5c579a5a272e1b1275917f1e4b2fd06ade9bc804
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-09-24 12:21:31 +1000

    Rename SendNotification

---
+++ lib/iodized/notification.ex
@@ -0,0 +1,37 @@
+defmodule Iodized.Notification do
+
+  @persistence Iodized.WebhookPersistence.Mnesia
+
+  def notify_event(event, message) do
+    {:ok, webhooks} = @persistence.all()
+
+    replace_message = fn url ->
+      encoded_message = URI.encode(message)
+      String.replace(url, "{{message}}", encoded_message)
+    end
+
+    webhooks
+      |> Enum.filter(fn hook -> hook.event_type == event end)
+      |> Enum.map(fn hook -> hook.url end)
+      |> Enum.map(replace_message)
+      |> Enum.map(&call_url/1)
+  end
+
+  defp call_url(http_url) do
+    IO.puts("Calling webhook " <> http_url)
+
+    HTTPoison.start
+
+    case HTTPoison.get(http_url) do
+      %HTTPoison.Response{status_code: 200, body: body} ->
+        IO.puts "Notify received body " <> body
+      %HTTPoison.Response{status_code: 302, body: _body, headers: %{"Location" => location}} ->
+        call_url location
+      %HTTPoison.Response{status_code: 404} ->
+        IO.puts http_url <> " not found :("
+      _ ->
+        IO.puts " problem calling webhook " <> http_url
+    end
+  end
+
+end
